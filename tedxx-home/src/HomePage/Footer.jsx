344990f59e618677a55ec82f2f3f5d0772046347
Shiva419 <pondurthishiva19@gmial.com>
2024-10-04 18:07:28 +0530

    Footer changed

--- tedxx-home/src/HomePage/Footer.jsx
+++ tedxx-home/src/HomePage/Footer.jsx
@@ -20,24 +20,24 @@ const Footer = ({onSectionChange}) => {
         <div className="footer-section">
           <h3>Let's <span>Connect</span></h3>
           <ul className="footer-social">
-            <li><i className="fab fa-youtube"></i> Youtube</li>
-            <li><i className="fab fa-linkedin"></i> Linkedin</li>
-            <li><i className="fab fa-instagram"></i> Instagram</li>
-            <li><i className="fab fa-twitter"></i> Twitter</li>
+            <li className="hover:text-red-600"><i className="fab fa-youtube"></i> Youtube</li>
+            <li className="hover:text-blue-600"><i className="fab fa-linkedin"></i> Linkedin</li>
+            <li className="hover:text-pink-600"><i className="fab fa-instagram"></i> Instagram</li>
+            <li className="hover:text-blue-400"><i className="fab fa-twitter"></i> Twitter</li>
           </ul>
         </div>
 
         {/* Right Section - Contact Info */}
-        <div className="footer-section">
+        <div className="footer-section ">
           <h3>Contact Us</h3>
-          <p><i className="fas fa-envelope"></i> tedxvjit@vjit.ac.in</p>
-          <p><i className="fas fa-phone"></i> +91 9866487922</p>
-          <p>+91 8121403233</p>
-          <p>+91 8106993270</p>
+          <p className="hover:text-red-600"><i className="fas fa-envelope"></i> tedxvjit@vjit.ac.in</p>
+          <p className="hover:text-red-600"><i className="fas fa-phone"></i> +91 9866487922</p>
+          <p className="hover:text-red-600"><i className="fas fa-phone"></i>+91 8121403233</p>
+          <p className="hover:text-red-600"><i className="fas fa-phone"></i>+91 8106993270</p>
         </div>
       </div>
 
-      <div className="footer-bottom">
+      <div className="footer-bottom text-red-600">
         <p>Copyright © 2024 - TEDxVJIT. All Rights Reserved</p>
       </div>
     </footer>
